63bd236fcaacfb05549589b1c9f5a6a3b602d76e
Amos Wenger <amoswenger@gmail.com>
2019-05-17 21:19:25 +0200

    refactor a bit

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use async_timer::oneshot::*;
 use romio::tcp::{TcpListener, TcpStream};
 use std::time::Duration;
 
+use lavish_rpc as rpc;
+
 mod proto;
 mod support;
 
@@ -21,7 +23,8 @@ async fn sleep_ms(n: u64) {
 }
 
 fn main() {
-    executor::block_on(async {
+    let mut pool = executor::ThreadPool::new().unwrap();
+    pool.run(async {
         futures::future::join(client(), server()).await;
     });
 }
@@ -42,24 +45,8 @@ async fn server() -> Result<(), Box<dyn std::error::Error + 'static>> {
         let rpc_system = RpcSystem::new(conn);
         let (mut sink, mut _stream) = (rpc_system.sink, rpc_system.stream);
 
-        let text = "Lorem ipsum. To most of us, it’s a passage of
-                    meaningless Latin that fills websites or brochure layouts
-                    with text while waiting on writers to fill it with real copy.
-                    This is bad news for publishers. But if one of those
-                    publishers decided to use it themselves, they'd be getting
-                    it. When in doubt, try to find a copy that's hard to find, a
-                    better copy is available, or an original copy of the text.";
-        let mut lines: Vec<String> = text
-            .split("\n")
-            .map(|x| x.trim())
-            .collect::<Vec<_>>()
-            .join(" ")
-            .split(".")
-            .map(|x| x.trim().into())
-            .collect();
-        lines.reverse();
-
-        for line in &lines {
+        for line in &sample_lines() {
+            sleep_ms(300).await;
             let m = proto::Message::request(
                 0,
                 proto::Params::double_Print(proto::double::print::Params { s: line.clone() }),
@@ -76,7 +63,6 @@ async fn client() -> Result<(), Box<dyn std::error::Error + 'static>> {
     sleep_ms(100).await;
 
     println!("[client] hello");
-    sleep_ms(250).await;
 
     let addr = ADDR.parse()?;
     let conn = TcpStream::connect(&addr).await?;
@@ -89,9 +75,41 @@ async fn client() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let (_sink, mut stream) = (rpc_system.sink, rpc_system.stream);
 
     while let Some(m) = stream.next().await {
-        println!("[client] received message: {:#?}", m?);
+        match m? {
+            rpc::Message::Request { params, .. } => match params {
+                proto::Params::double_Print(params) => {
+                    println!("[client] {}", params.s);
+                }
+                _ => {
+                    println!("[client] request: {:#?}", params);
+                }
+            },
+            m => {
+                println!("[client] message: {:#?}", m);
+            }
+        }
     }
 
     println!("[client] is done receiving message, shutting down");
     Ok(())
+}
+
+fn sample_lines() -> Vec<String> {
+    let text = "Lorem ipsum. To most of us, it’s a passage of
+                    meaningless Latin that fills websites or brochure layouts
+                    with text while waiting on writers to fill it with real copy.
+                    This is bad news for publishers. But if one of those
+                    publishers decided to use it themselves, they'd be getting
+                    it. When in doubt, try to find a copy that's hard to find, a
+                    better copy is available, or an original copy of the text.";
+    let mut lines: Vec<String> = text
+        .split("\n")
+        .map(|x| x.trim())
+        .collect::<Vec<_>>()
+        .join(" ")
+        .split(".")
+        .map(|x| x.trim().into())
+        .collect();
+    lines.reverse();
+    lines
 }
